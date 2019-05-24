b5d52ad8864c3887adde965eb92c344ba8232119
Adarsh Pal <palad@iitk.ac.in>
2019-05-24 20:41:51 +0530

    New Commit

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 # Create your models here.
 from django.conf import settings
+import datetime
 from django.utils import timezone
 class Post(models.Model):
     author=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -9,7 +10,7 @@ class Post(models.Model):
     created_date=models.DateTimeField(default=timezone.now)
     published_date=models.DateTimeField(blank=True,null=True)
     def publish(self):
-        self.published_date=datetime.now()
+        self.published_date=timezone.now()
         self.save()
     def __str__(self):
         return self.title
